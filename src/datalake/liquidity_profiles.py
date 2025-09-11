0a0f3e4479cb920fb91f3f46a776ed6ef98ac987
Nelsonf09 <nelsonfcardona@gmail.com>
2025-09-10 19:34:09 -0500

    feat: add crypto liquidity profiles

---
+++ src/datalake/liquidity_profiles.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+LIQUIDITY_PROFILES_CRYPTO = {
+    "daily_open_utc":   {"tz": "UTC",                "start": "00:00", "minutes": 5},
+    "us_equity_open":   {"tz": "America/New_York",   "start": "09:30", "minutes": 5},
+    "asia_open":        {"tz": "Asia/Tokyo",         "start": "09:00", "minutes": 5}
+}
+
+DEFAULT_CRYPTO_PROFILE = "us_equity_open"
